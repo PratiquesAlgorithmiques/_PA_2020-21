8251977a5a337f9c3c0ce8e35db075cd83261536
PratiquesAlgorithmiques <pratiquesalgorithmiques@gmail.com>
2020-11-18 19:03:26 +0100

    exemple p5 avec arguments

--- _04_16nov20_workshop_sarah_garcin_day2/_04_pagedjs-p5_comments_with_args/script.js
+++ _04_16nov20_workshop_sarah_garcin_day2/_04_pagedjs-p5_comments_with_args/script.js
@@ -30,11 +30,12 @@ class createPages extends Paged.Handler{
 				var container 	= page.element.querySelector('section');			// trouver l'élément section sur la page actuelle
 				var div 		= document.createElement('div');					// créer un div (pas encore intégré)
 				div.id 			= "myContainer" + page.position						// donne un id, si page.position = 0 ce serait myContainer0, etc.
-						
-
-				p5one(div)															// appeler la fonction p5one, envoyer le div (myContainer0)
-				// p5two(div)														// vous pouvez créer d'autres fonctions et les appeler
-
+				
+				
+				// LE NOUVEAUTÉ EST ICI (on passe une référence au div et aussi un mot qui sera utilisé dans la fonction p5one)
+				p5one(div, "rect")
+				p5one(div, "ellipse")												// appeler la fonction p5one, envoyer le div (myContainer0)
+				
 				// Crée un paragraphe, lui donne un id, appelle getText() (plus bas) et change le html du paragraphe
 				// le rajoute au div (après le p5)
 				var para 		= document.createElement('p');
@@ -49,43 +50,54 @@ class createPages extends Paged.Handler{
 }
 Paged.registerHandlers(createPages);
 
-function p5one(div) {											// dans une fonction qu'on appelle au dessus
-	const s = ( p ) => {										// syntaxe pour l'instance mode
 
-		// code p5 en instance mode (note le p. devant tous les fonctions p5 (en vert))
-		// https://www.youtube.com/watch?v=Su792jEauZg&feature=youtu.be
-		// https://p5js.org/reference/#/p5/p5
+// quelleforme div et quelleforme sont des arguments qu'on fait passer au moment où on appelle cette fonction
+// cela permet de personaliser l'instance, le if plus bas l'utilise pour choisir de montrer un cercle ou un carré
 
-		let x 		= 100.0; 
-		let y 		= p.random(0, 200); 
-		let speed 	= p.random(1, 10); 							// random float entre 1 et 10
+function p5one(div, quelleforme) {											// dans une fonction qu'on appelle au dessus
+	const s = ( p ) => {													// syntaxe pour l'instance mode
 
 		p.setup = function() {
 		  p.createCanvas(400, 200);
-		  p.fill(p.random(255), p.random(255), p.random(255))	// random color
+		  p.fill(255, 0, 0)	// random color
 		};
 	  
 		p.draw = function() {
 		  p.background(100);
-		  
-		  x += speed; 											// augmenter par speed 
 
-		  if(x > p.width) {										// si plus grand que la largeur, x = 0
-			  x = 0; 
+		  if (quelleforme == "rect") {										// prise de décision
+		  	p.rect(100, 100, 50, 50);										// si (quelleforme == "rect") = true 
+		  } else {															// si (quelleforme == "rect") = false
+			p.ellipse(100, 100, 50, 50);
 		  }
 
-		  p.ellipse(x, y, 50, 50);
 		};
-	};
+	}
 
 	let myp5 = new p5(s, div);
 }
 
+// function p5two(div) {											// dans une fonction qu'on appelle au dessus
+// 	const s = ( p ) => {										// syntaxe pour l'instance mode
+
+// 		p.setup = function() {
+// 		  p.createCanvas(400, 200);
+// 		  p.fill(255, 0, 0)	// random color
+// 		};
+	  
+// 		p.draw = function() {
+// 		  p.background(100);
+// 		  p.ellipse(100, 100, 50, 50);
+// 		};
+// 	}
+
+// 	let myp5 = new p5(s, div);
+// }
 
 
 // fonction javascript qui choisit aléatoirement un mot depuis un array
 function getText(){
 	var couleur = ["green", "dark", "black", "pure", "red", "blue", "yellow", "saphir", "brown"];
 
-	return couleur[Math.floor(Math.random()* couleur.length)];
+	return couleur[Math.floor(Math.random() * couleur.length)];
 }
